b5547cbdfe3a8cab848a54ffa29b28fc1a6a033d
BenTapia15 <ben.tapiab@duocuc.cl>
2025-06-25 10:31:13 -0400

    Prueba 04

---
+++ BenjaminTapia.py
@@ -0,0 +1,9 @@
+def menu ():
+    print ("*"*70)
+    print ("1.- Ingresar usuario.")
+    print ("2.- Buscar usuario.")
+    print ("3.- Eliminar usuario.")
+    print ("4.- Salir.")
+    opcion=input ("Ingrese opción: ")
+    print ("*"*70)
+    return opcion
